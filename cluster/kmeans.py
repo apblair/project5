fd219ef3a198107b70315bc09fe520ffcc9f395b
Andrew Blair <andrew.blair@ucsf.edu>
2022-02-08 16:07:23 -0800

    v0.1.0 KMeans _init_centroids, calculate_mse, fit methods

--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.distance import cdist
+from utils import *
 
 class KMeans:
     def __init__(
@@ -19,7 +20,34 @@ class KMeans:
             max_iter: int
                 the maximum number of iterations before quitting model fit
         """
+        self.k = k
+        self.metric = metric
+        self.tol = tol
+        self.max_iter = max_iter
     
+    def _init_centroids(self):
+        """
+        Compute the initial centroids
+
+        References:
+        -----------
+        1. https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+        2. https://www.askpython.com/python/examples/k-means-clustering-from-scratch
+        """
+        self._centroids = self._mat[np.random.choice(self._mat.shape[0], self.k, replace=False),:]
+        self._distances = cdist(self._mat, self._centroids, self.metric)
+        self._labels = np.argmin(self._distances, axis=1)
+    
+    def _calculate_mse(self):
+        """
+        """
+        points_to_centroid = np.array(list(map(lambda label: self._centroids[label], self._labels)))
+        distances = np.diag(cdist(self._mat, points_to_centroid, metric=self.metric))
+        distances_squared = np.square(distances)
+        mse = distances_squared.mean()
+        return mse
+        
+
     def fit(self, mat: np.ndarray):
         """
         fits the kmeans algorithm onto a provided 2D matrix
@@ -27,7 +55,20 @@ class KMeans:
         inputs: 
             mat: np.ndarray
                 A 2D matrix where the rows are observations and columns are features
+        
+        References
+        ----------
+        1. https://blog.paperspace.com/speed-up-kmeans-numpy-vectorization-broadcasting-profiling/
         """
+        self._mat = mat
+        self._mse = []
+        for iter_step in range(self.max_iter):
+            self._init_centroids()
+            for k_cluster in range(self.k):
+                self._centroids[k_cluster,:] = np.mean(self._mat[self._labels == k_cluster,:],axis = 0)
+            self._mse.append(self._calculate_mse())
+            print(self._mse)
+            break        
 
     def predict(self, mat: np.ndarray) -> np.ndarray:
         """
@@ -59,3 +100,9 @@ class KMeans:
             np.ndarray
                 a `k x m` 2D matrix representing the cluster centroids of the fit model
         """
+
+mat, labels = make_clusters()
+# print(mat)
+# print(labels)
+
+KMeans(k=3).fit(mat)
